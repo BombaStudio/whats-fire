e523f26b5799b5d40e49ee9e10269aaac0ea2da9
BombaStudio <bombaoyun111@gmail.com>
2020-04-09 13:45:44 +0300

    commit

--- whatsfire.py
+++ whatsfire.py
@@ -6,49 +6,42 @@ help = """
 """
 inp = "[whats fire]"
 
-def bomb():
-    driver = webdriver.Chrome()
+def bomb(path):
+    driver = webdriver.Chrome(executable_path=path)
     driver.get('https://web.whatsapp.com/')
-    if sys.version_info.major == 3:
-        victim = input("enter victim:")
-        mess = input("message:")
-        count = int(input("set count:"))
-    if sys.version_info.major == 2:
-        victim = raw_input("enter victim:")
-        mess = raw_input("message:")
-        count = input("set count:")
+    victim = input("enter victim:")
+    mess = input("message:")
+    count = int(input("set count:"))
     kullan = driver.find_element_by_xpath('//span[@title = "{}"]'.format(victim))
     kullan.click()
-    messbox = driver.find_element_by_class_name('_2bXVy')
+    messbox = driver.find_element_by_class_name('_3u328')
     for i in range(count):
         messbox.send_keys(msg)
-        button = driver.find_element_by_class_name('_2lktd')
+        button = driver.find_element_by_class_name('_3M-N-')
         button.click()
     print("mission passed :D")
 def main(x):
     if x == "help":
         print(help)
     if x == "1" or x == "bomber":
-        bomb()
-def main2():
-    while True:
-        x = raw_input(inp)
-        main(x)
+        path = input("enter partch:")
+        bomb(path)
 def main3():
     while True:
         x = input(inp)
         main(x)
 def setup():
     if sys.version_info.major == 2:
-        system("pip2 install selenium")
+        print("python version is not 3 please install python3")
     elif sys.version_info.major == 3:
         system("pip3 install selenium")
+        print("selenium is installed. please install webdriver.")
 if __name__ == '__main__':
     try:
         from selenium import webdriver
     except:
         setup()
     if sys.version_info.major == 2:
-        main2()
+        print("python version is not 3 please install python3")
     elif sys.version_info.major == 3:
         main3()
